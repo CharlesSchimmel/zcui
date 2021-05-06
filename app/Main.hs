120770a476c390a0645a40e3bc1f98f894f37409
CharlesSchimmel <charlesschimmel@gmail.com>
2021-05-05 21:55:42 -0500

    more cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -18,83 +18,17 @@ import           Turtle
 
 main :: IO ()
 main = do
-    args   <- doParseArgs
-    optsOk <- verifyOptions args
-    let env = mkEnv <$> optsOk
+    config <- getConfig
+    let env = mkEnv <$> config
     either T.putStrLn runProgram env
 
 runProgram :: Env -> IO ()
 runProgram env = do
-    either T.putStrLn pure =<< runExceptT (runReaderT (runApp zcuiM) env)
+    runResult <- runExceptT (runReaderT (runApp zcuiM) env)
+    either T.putStrLn pure runResult
 
 mkEnv :: Config -> Env
 mkEnv conf = Env conf oldLogger
 
 oldLogger :: MonadIO io => Text -> io ()
 oldLogger = liftIO . T.putStrLn
-
-verifyOptions :: Arguments -> IO (Either Text Config)
-verifyOptions Arguments {..} = do
-    music   <- testMusicDir argMusicDir
-    archive <- testArchive argArchiveOptions
-    let convOpts = testConversionOptions argConversionOptions
-    pure $ Config <$> music <*> archive <*> convOpts
-
-testConversionOptions copts@(ConversionOptions (Bitrate bitrate)) =
-    if bitrate > 32 && bitrate < 512
-        then pure copts
-        else Left "Bitrate must be between 32-512"
-
-testMusicDir :: MusicDir -> IO (Either Text MusicDir)
-testMusicDir musicDir@(MusicDir path) =
-    (fmap $ const musicDir) <$> testdir' path
-
-testArchive :: ArchiveOptions -> IO (Either Text ArchiveOptions)
-testArchive NoArchive = pure . pure $ NoArchive
-testArchive opts@(MoveArchive (ArchiveDir archDir)) =
-    (fmap $ const opts) <$> testdir' archDir
-testArchive opts@(ZipArchive (ArchiveDir archDir)) =
-    (fmap $ const opts) <$> testdir' archDir
-
-testdir' :: FilePath -> IO (Either Text FilePath)
-testdir' path = do
-    pathExists <- testdir path
-    if pathExists
-        then pure . Right $ path
-        else
-            pure
-            . Left
-            . T.append "Could not find path: "
-            . T.pack
-            . show
-            $ path
-
--- normalizePath :: FilePath -> Shell (Either Text FilePath)
--- normalizePath path
---     | relative path =   (maybe (Left "Failed to absolutize path") Right)
---     <$> homeRelative path
---     | otherwise = pure . pure $ path
-
--- absolutize :: FilePath -> Shell (Either Text FilePath)
--- absolutize = 
-
--- absolutize' :: FilePath -> FilePath -> Maybe FilePath
--- absolutize' workingDir targetPath =
---     where withoutDots = strip
-
--- noDubs :: FilePath -> FilePath
--- noDubs path = maybe path noDubs $ stripPrefix "../" path
-
--- noDot :: FilePath -> FilePath
--- noDots path = maybe path noDot $ stripPrefix "./" path
-
--- homeRelative :: FilePath -> Shell (Maybe FilePath)
--- homeRelative path = do
---     home <- need "$HOME"
---     let homePath = fromText <$> home
---     return $ homePath >>= flip homeRelative' path
-
--- homeRelative' :: FilePath -> FilePath -> Maybe FilePath
--- homeRelative' homeDir targetDir = (homeDir </>) <$> withoutTilde
---     where withoutTilde = stripPrefix "~/" targetDir
-
--- app/Options.hs
+++ app/Options.hs
@@ -1,13 +1,18 @@
 module Options
   ( doParseArgs
   , Arguments(..)
+  , getConfig
   )
 where
 
 import           Types
 
+import           Data.Text                     as T
+import           Prelude                 hiding ( FilePath )
 import           Turtle
 
+getConfig :: IO (Either Text Config)
+getConfig = doParseArgs >>= verifyOptions
 
 data Arguments = Arguments
   { argMusicDir          :: MusicDir
@@ -20,14 +25,9 @@ doParseArgs = options "Zip Convert Update Import" argParse
 
 argParse = Arguments <$> musicDirParse <*> archiveOpts <*> conversionsOpts
 
+musicDirParse :: Parser MusicDir
 musicDirParse =
   MusicDir <$> optPath "music-dir" 'm' "Music directory to search"
-
-doMove = switch "move" 'v' "Move files to archive"
-
-doZip = switch "zip" 'z' "Zip files to archive"
-
-archDir = ArchiveDir <$> optPath "archive-dir" 'a' "Archive directory"
 
 moveArch :: Parser ArchiveOptions
 moveArch = MoveArchive . ArchiveDir <$> optPath
@@ -57,3 +57,33 @@ bitrateParse =
     <|> pure defaultBitrate
 
 defaultBitrate = Bitrate 128
+
+verifyOptions :: Arguments -> IO (Either Text Config)
+verifyOptions Arguments {..} = do
+  music   <- testMusicDir argMusicDir
+  archive <- testArchive argArchiveOptions
+  let convOpts = testConversionOptions argConversionOptions
+  pure $ Config <$> music <*> archive <*> convOpts
+
+testConversionOptions copts@(ConversionOptions (Bitrate bitrate)) =
+  if bitrate > 32 && bitrate < 512
+    then pure copts
+    else Left "Bitrate must be between 32-512"
+
+testMusicDir :: MusicDir -> IO (Either Text MusicDir)
+testMusicDir musicDir@(MusicDir path) =
+  (fmap $ const musicDir) <$> testdir' path
+
+testArchive :: ArchiveOptions -> IO (Either Text ArchiveOptions)
+testArchive NoArchive = pure . pure $ NoArchive
+testArchive opts@(MoveArchive (ArchiveDir archDir)) =
+  (fmap $ const opts) <$> testdir' archDir
+testArchive opts@(ZipArchive (ArchiveDir archDir)) =
+  (fmap $ const opts) <$> testdir' archDir
+
+testdir' :: FilePath -> IO (Either Text FilePath)
+testdir' path = do
+  pathExists <- testdir path
+  if pathExists
+    then pure . Right $ path
+    else pure . Left . T.append "Could not find path: " . T.pack . show $ path
